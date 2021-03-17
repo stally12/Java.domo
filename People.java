8ca37bf0bf07f08ecb7abba3b48068de661d1f62
stally12 <3014384242@qq.com>
2021-03-17 20:59:34 +0800

    完成了people类的定义

---
+++ People.java
@@ -0,0 +1,11 @@
+package 第八课;
+
+import javax.print.DocFlavor;
+
+public class People {
+    String name;
+    public void speak(String i) {
+        System.out.println(name+"："+i);
+    }
+
+}
---
+++ PeopleDemo.java
@@ -0,0 +1,13 @@
+package 第八课;
+
+public class PeopleDemo {
+    public static void main(String[] args) {
+        People xh=new People();
+        xh.name="小红";
+        xh.speak("你好，世界。");
+        People xb=new People();
+        xb.name="小白";
+        xb.speak("世界那么大。");
+
+    }
+}
